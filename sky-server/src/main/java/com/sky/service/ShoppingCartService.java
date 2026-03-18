4162f0e4267005957f808995e2cd8582a40cbb7d
gfy_goodforu <gfy050922@gmail.com>
2026-03-18 20:57:28 +0800

    清空购物车及删除购物车中一个商品业务代码开发

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -21,4 +21,21 @@ public interface ShoppingCartService {
      */
     List<ShoppingCart> showShoppingCart();
 
+
+
+    /**
+     * 清空购物车
+     * @return
+     */
+    void cleanShoppingCart();
+
+
+
+
+
+    /**
+     * 删除购物车中一个商品
+     * @param shoppingCartDTO
+     */
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
 }
