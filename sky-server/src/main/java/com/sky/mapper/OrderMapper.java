a5fd190ad97471c2a37fdae2fd40faf00027da1a
gfy_goodforu <gfy050922@gmail.com>
2026-03-21 14:25:19 +0800

    订单数据统计功能代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,6 +2,7 @@ package com.sky.mapper;
 
 
 import com.github.pagehelper.Page;
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
@@ -17,13 +18,15 @@ public interface OrderMapper {
 
     /**
      * 插入订单数据
+     *
      * @param orders
      */
-    void insert (Orders orders);
+    void insert(Orders orders);
 
 
     /**
      * 根据订单号查询订单
+     *
      * @param orderNumber
      */
     @Select("select * from orders where number = #{orderNumber}")
@@ -31,6 +34,7 @@ public interface OrderMapper {
 
     /**
      * 修改订单信息
+     *
      * @param orders
      */
     void update(Orders orders);
@@ -38,6 +42,7 @@ public interface OrderMapper {
 
     /**
      * 用于替换微信支付更新数据库状态的问题
+     *
      * @param orderStatus
      * @param orderPaidStatus
      */
@@ -46,17 +51,17 @@ public interface OrderMapper {
     void updateStatus(Integer orderStatus, Integer orderPaidStatus, LocalDateTime check_out_time, String orderNumber);
 
 
-
     /**
      * 分页条件查询并按下单时间排序
+     *
      * @param ordersPageQueryDTO
      */
     Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
 
 
-
     /**
      * 根据id查询订单
+     *
      * @param id
      */
     @Select("select * from orders where id=#{id}")
@@ -65,6 +70,7 @@ public interface OrderMapper {
 
     /**
      * 根据状态统计订单数量
+     *
      * @param status
      */
     @Select("select count(id) from orders where status = #{status}")
@@ -73,6 +79,7 @@ public interface OrderMapper {
 
     /**
      * 根据订单状态和下单时间查询订单
+     *
      * @param status
      * @param orderTime
      * @return
@@ -83,11 +90,20 @@ public interface OrderMapper {
 
     /**
      * 根据动态条件统计营业额数据
+     *
      * @param map
      * @return
      */
     Double sumByMap(Map<String, Object> map);
 
+
+    /**
+     * 根据动态条件统计订单数量
+     *
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 
 
 }
